547dc703c65875ad5ba1215f6da47cc1d6072136
Kohei Watanabe <kou029w@gmail.com>
2020-04-09 23:19:47 +0900

    tottori: 新型コロナ受診相談窓口相談件数グラフ追加

--- tool/main.js
+++ tool/main.js
@@ -7,46 +7,108 @@ const areaName = 'tottori'
 const defaultLanguageCode = 'ja-JP'
 const defaultTimezone = '+0900'
 const date = new Date().toLocaleString(defaultLanguageCode)
-const templateJson = fs.readFileSync('../data/data.json.template')
-const template = JSON.parse(templateJson)
 
-const inspectionDataset = fs.createReadStream(
-  `downloads/${areaName}/inspection.csv`
-)
+async function importInspectionDataset() {
+  const inspectionDataset = fs.createReadStream(
+    `downloads/${areaName}/inspection.csv`
+  )
 
-const inspectionParser = csv.parse({ columns: true })
-const inspectionsSummary = {
-  date,
-  data: {
-    [areaName]: []
-  },
-  labels: []
-}
-const patientsSummary = {
-  date,
-  data: []
-}
-
-inspectionParser.on('readable', () => {
-  let data
-  while ((data = inspectionParser.read())) {
-    inspectionsSummary.labels.push(`${data.月}/${data.日}`)
-    inspectionsSummary.data[areaName].push(Number(data.検査件数))
-    patientsSummary.data.push({
-      日付: [
-        `${data.年}-${`0${data.月}`.slice(-2)}-${`0${data.日}`.slice(-2)}`,
-        `00:00:00${defaultTimezone}`
-      ].join('T'),
-      小計: Number(data.陽性)
-    })
+  const inspectionParser = csv.parse({ columns: true })
+  const inspectionsSummary = {
+    date,
+    data: {
+      [areaName]: []
+    },
+    labels: []
   }
-})
-inspectionParser.on('end', () => {
+  const patientsSummary = {
+    date,
+    data: []
+  }
+
+  inspectionParser.on('readable', () => {
+    let data
+    while ((data = inspectionParser.read())) {
+      inspectionsSummary.labels.push(`${data.月}/${data.日}`)
+      inspectionsSummary.data[areaName].push(Number(data.検査件数))
+      patientsSummary.data.push({
+        日付: [
+          `${data.年}-${`0${data.月}`.slice(-2)}-${`0${data.日}`.slice(-2)}`,
+          `00:00:00${defaultTimezone}`
+        ].join('T'),
+        小計: Number(data.陽性)
+      })
+    }
+  })
+
+  const promise = new Promise(resolve => {
+    inspectionParser.on('end', resolve)
+  })
+
+  inspectionDataset.pipe(inspectionParser)
+
+  await promise
+
+  return { inspectionsSummary, patientsSummary }
+}
+
+async function importQuerentDataset() {
+  const inspectionDataset = fs.createReadStream(
+    `downloads/${areaName}/querent.csv`
+  )
+
+  const querentsParser = csv.parse({ columns: true })
+  const querents = {
+    date,
+    data: []
+  }
+
+  querentsParser.on('readable', () => {
+    let data
+    while ((data = querentsParser.read())) {
+      querents.data.push({
+        日付: [
+          `${data.年}-${`0${data.月}`.slice(-2)}-${`0${data.日}`.slice(-2)}`,
+          `00:00:00${defaultTimezone}`
+        ].join('T'),
+        小計: Number(data.相談件数)
+      })
+    }
+  })
+
+  const promise = new Promise(resolve => {
+    querentsParser.on('end', resolve)
+  })
+
+  inspectionDataset.pipe(querentsParser)
+
+  await promise
+
+  return { querents }
+}
+
+async function main() {
+  const templateJson = fs.readFileSync('../data/data.json.template')
+  const template = JSON.parse(templateJson)
+
+  template.lastUpdate = date
+  template.patients.date = date
+
+  const {
+    inspectionsSummary,
+    patientsSummary
+  } = await importInspectionDataset()
+
   template.inspections_summary = inspectionsSummary
   template.patients_summary = patientsSummary
-  template.patients.date = date
-  template.lastUpdate = date
+
+  const { querents } = await importQuerentDataset()
+
+  template.querents = querents
+
   fs.writeFileSync(outputPath, JSON.stringify(template, null, 4))
+
   console.log('success')
-})
-inspectionDataset.pipe(inspectionParser)
+}
+
+main()
